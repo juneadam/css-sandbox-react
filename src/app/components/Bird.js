cd7d9024004f029beea1b43ab4557b1a10f04ea6
June Balter <junedoeswork@gmail.com>
2023-07-05 21:31:00 -0500

    bird component can now move when user presses up and down arrows on the keyboard

--- src/app/components/Bird.js
+++ src/app/components/Bird.js
@@ -1,8 +1,53 @@
+'use client'
+
 import styles from "./Bird.module.css";
+import { useRef } from "react";
+
+const useBird = () => {
+  const bird = useRef(0);
+
+  let moveBy = 50;
+
+  window.addEventListener('load', () => {
+    const winHeight = window.innerHeight
+    // console.log(winHeight)
+    const winWidth = window.innerWidth
+    const birdTopPos0 = `${winHeight/2}px`
+    // console.log(birdTopPos0)
+    const birdLeftPos0 = `${winWidth/6}px`
+    bird.current.style.position = 'absolute';
+    bird.current.style.left = `${birdLeftPos0}`;
+    bird.current.style.top = `${birdTopPos0}`;
+});
+
+  window.addEventListener("keyup", (e) => {
+    switch (e.key) {
+    //   case "ArrowLeft":
+    //     bird.current.style.left = parseInt(bird.current.style.left) - moveBy + "px";
+    //     break;
+    //   case "ArrowRight":
+    //     bird.current.style.left = parseInt(bird.current.style.left) + moveBy + "px";
+    //     break;
+      case "ArrowUp":
+        bird.current.style.top = parseInt(bird.current.style.top) - moveBy + "px";
+        console.log(bird.current.style.top)
+        break;
+      case "ArrowDown":
+        bird.current.style.top = parseInt(bird.current.style.top) + moveBy + "px";
+        console.log(bird.current.style.top)
+        break;
+    }
+  });
+  return bird;
+};
 
 export default function Bird() {
+  let bird = useBird();
   return (
-    <div className={`${styles.frendWrapper} relative overflow-hidden`}>
+    <div
+      ref={bird}
+      className={`${styles.frendWrapper} relative overflow-hidden`}
+    >
       <div className={`${styles.frend} absolute`}></div>
     </div>
   );
